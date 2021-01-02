79621d2c12a65c5347fc2464344d3edfd0f7f995
challe-git <76874224+challe-git@users.noreply.github.com>
2021-01-02 15:34:26 +0100

    Create calc.py
    This is a calculator in python, feel free to take it as REFRENCE.
    DO NOT COPY, PASTE!

---
+++ calc.py
@@ -0,0 +1,16 @@
+value1 = int(input("Enter a number: "))
+operator = input("Enter a operator: ")
+value2 = int(input("Enter a number: "))
+
+value1 = v1
+operator = op
+value2 = v2
+
+if op == "+":
+  print(v1 + v2)
+if op == "-":
+  print(v1 - v2)
+if op == "*":
+  print(v1 * v2)
+if op == "/":
+  print(v1 / v2)
